f6828096726ccfb30009c7a7815c3ea55592e6be
Philip-Ngui <ngui9737@saskpolytech.ca>
2025-10-22 17:42:31 -0600

    Files

---
+++ BankAccountPractice/src/BankAccount/Person.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BankAccount;
+
+/**
+ *
+ * @author Philip Ngui
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private char middleInitial;
+
+}
